3dc5c6645b6d90f48c0ee78aef697c2cc76b559f
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-24 13:30:25 +0200

    Create endpoint with Class-Based Views for Actor

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from theatre_service.views import GenreList, GenreDetail
+from theatre_service.views import (
+   GenreList,
+   GenreDetail,
+   ActorList,
+   ActorDetail,
+)
 
 
 
 urlpatterns = [
    path("genre/", GenreList.as_view(), name="genre-list"),
    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+   path('actors/', ActorList.as_view(), name='actor-create'),
+   path('actors/<int:pk>/', ActorDetail.as_view(), name='actor-detail'),
 ]
 
 app_name = "theatre_service"
--- theatre_service/views.py
+++ theatre_service/views.py
@@ -1,7 +1,7 @@
-from rest_framework.views import APIView
 from django.http import JsonResponse
-from .models import Genre
-from .serializers import GenreSerializer
+from rest_framework.views import APIView
+from .models import Genre, Actor
+from .serializers import GenreSerializer, ActorSerializer
 
 
 class GenreList(APIView):
@@ -39,7 +39,6 @@ class GenreDetail(APIView):
             return JsonResponse({'data': serializer.data})
         return JsonResponse({'errors': serializer.errors}, status=400)
 
-
     def delete(self, request, pk):
         try:
             genre = Genre.objects.get(pk=pk)
@@ -48,3 +47,49 @@ class GenreDetail(APIView):
 
         genre.delete()
         return JsonResponse({}, status=204)
+
+
+class ActorList(APIView):
+    def get(self, request):
+        actors = Actor.objects.all()
+        serializer = ActorSerializer(actors, many=True)
+        return JsonResponse({'data': serializer.data})
+
+    def post(self, request):
+        serializer = ActorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'data': serializer.data}, status=201)
+        return JsonResponse({'errors': serializer.errors}, status=400)
+
+
+class ActorDetail(APIView):
+    def get(self, request, pk):
+        try:
+            actor = Actor.objects.get(pk=pk)
+        except Actor.DoesNotExist:
+            return JsonResponse({"detail": "Not found."}, status=404)
+        serializer = ActorSerializer(actor)
+        return JsonResponse({'data': serializer.data})
+
+    def put(self, request, pk):
+        try:
+            actor = Actor.objects.get(pk=pk)
+        except Actor.DoesNotExist:
+            return JsonResponse({"detail": "Not found."}, status=404)
+
+        serializer = ActorSerializer(actor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'data': serializer.data})
+        return JsonResponse({'errors': serializer.errors}, status=400)
+
+    def delete(self, request, pk):
+        try:
+            actor = Actor.objects.get(pk=pk)
+        except Actor.DoesNotExist:
+            return JsonResponse({"detail": "Not found."}, status=404)
+
+        actor.delete()
+        return JsonResponse({}, status=204)
+
