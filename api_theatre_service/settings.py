50ff774daadc63c951d6a36e4d07c41182112719
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-04 13:01:48 +0200

    Update settings with no visible secret key

--- api_theatre_service/settings.py
+++ api_theatre_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-89r(seuui$9xbvp8-w8-o=ybybtuollxjt@_tpo%7x1&ubrve-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
