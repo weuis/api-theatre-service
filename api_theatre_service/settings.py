3500ddfb5abb008b8bb1a60e913f3da26d0f5586
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-04 14:03:20 +0200

    Add Docker support

--- api_theatre_service/settings.py
+++ api_theatre_service/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'api_theatre_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
+        'USER': os.getenv('POSTGRES_USER', 'user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
